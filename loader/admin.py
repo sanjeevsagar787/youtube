d038032b84a48180f675925c0853ed984218522d
Sanjeev Sagar <62535038+sanjeevsagar787@users.noreply.github.com>
2020-08-14 09:20:38 +0530

    Update admin.py

--- loader/admin.py
+++ loader/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from embed_video.admin import AdminVideoMixin
-from .models import Item
+
 
 class MyModelAdmin(AdminVideoMixin, loader.ModelAdmin):
     pass
