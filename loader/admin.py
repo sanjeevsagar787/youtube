1ba388218c4cc2e0d2d8748c515aa979498a8879
Sanjeev Sagar <62535038+sanjeevsagar787@users.noreply.github.com>
2020-08-14 09:17:18 +0530

    Update admin.py

--- loader/admin.py
+++ loader/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from embed_video.admin import AdminVideoMixin
 from .models import Item
 
-class MyModelAdmin(AdminVideoMixin, admin.ModelAdmin):
+class MyModelAdmin(AdminVideoMixin, loader.ModelAdmin):
     pass
 
 admin.site.register(Item, MyModelAdmin)
